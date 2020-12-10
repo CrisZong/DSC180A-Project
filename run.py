10b8700d8b2e3a3153ee4fa527198bcff947f3bb
nlin2 <nil021@ucsd.edu>
2020-12-09 21:19:54 -0800

    final

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from src.data.etl import fetch_and_save,transform_data
 from src.model.model import loss_p,loss_s,loss_ps
-from src.analysis.analysis import showLoss
+from src.analysis.analysis import showLoss, makePlot
 import pandas as pd
 import sys
 import json
-
+import os
 
 def main(targets):
 
@@ -14,26 +14,31 @@ def main(targets):
             data_cfg = json.load(fh)
 
         # make the data target
+        os.mkdir("data")
         fetch_and_save(data_cfg["links"][0],data_cfg["save_name"][0])
         transform_data("data/covid_time_series.xls",'data/cleaned_series.csv')
     if 'correlations' in targets: 
         with open('config/model-params.json') as fh:
             corr_cfg = json.load(fh)
         covid_series = pd.read_csv('data/cleaned_series.csv')
-        showLoss(covid_series["cases_specimen"],
+        offsetDict = showLoss(covid_series["cases_specimen"],
                 covid_series["cases_reported"],
                 corr_cfg["correlations"][0],
                 corr_cfg["correlations"][1],
-                [loss_p,loss_s,loss_ps])
+                [loss_ps,loss_p,loss_s])
+        makePlot(offsetDict["sp"], offsetDict["p"], offsetDict["s"], "correlationsPlot.png", covid_series)
+    
     if 'test' in targets: 
         with open('config/model-params.json') as fh:
             corr_cfg = json.load(fh)
         covid_series = pd.read_csv('test/testdata/test_series.csv')
-        showLoss(covid_series["cases_specimen"],
+        offsetDict = showLoss(covid_series["cases_specimen"],
                 covid_series["cases_reported"],
                 corr_cfg["correlations"][0],
                 corr_cfg["correlations"][1],
-                [loss_p,loss_s,loss_ps])
+                [loss_ps,loss_p,loss_s])
+        covid_series["cases_specimen"] = covid_series["cases_specimen"] / 10000
+        makePlot(offsetDict["sp"], offsetDict["p"], offsetDict["s"], "correlationsTestPlot.png", covid_series)
     return
 
 
